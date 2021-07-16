723c455d8468f12ae56db1ae5f2b18745c258e40
Ravi <Ravi@Agadia-05>
2021-07-16 18:20:53 +0530

    Test pr git

---
+++ ArifactS/src/main/java/TestGroupSagar/ArifactS/App.java
@@ -0,0 +1,13 @@
+package TestGroupSagar.ArifactS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ArifactS/src/test/java/TestGroupSagar/ArifactS/Samplecla.java
@@ -0,0 +1,10 @@
+package TestGroupSagar.ArifactS;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Samplecla {
+	
+	WebDriver driver = new FirefoxDriver();
+
+}
